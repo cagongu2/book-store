d5226cd2611ceb7dae606f03188ac7564b28c0a7
Thai <hophucthai03@gmail.com>
2026-07-23 12:31:30 +0700

    chore(frontend): update tsconfig and configs

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -12,8 +12,7 @@ export default [
       ecmaVersion: 2020,
       globals: {
         ...globals.browser, 
-        ...globals.node,    
-        ...globals.jest
+        ...globals.node
       },
       parserOptions: {
         ecmaVersion: 'latest',
