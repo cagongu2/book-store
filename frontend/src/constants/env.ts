e9c1c19968b9e34c3c66dec4dc209e7eb3d1fa2c
Thai <hophucthai03@gmail.com>
2026-07-23 13:13:11 +0700

    chore(frontend): remove old frontend to rebuild

--- frontend/src/constants/env.ts
+++
@@ -1,3 +0,0 @@
-export const ENV = {
-  END_POINT: import.meta.env.VITE_API_URL ?? "",
-} as const;
--- frontend/src/utils/fileExtention.ts
+++
@@ -1,4 +0,0 @@
-export const IMPORT_EXCEL = ".xls,.xlsx"
-export const IMPORT_WORD = ".doc,.docx"
-export const IMPORT_PDF = ".pdf"
-export const IMPORT_IMAGE = ".jpg,.jpeg,.png,.gif,.bmp,.webp,.svg,.tiff,.ico"
